95591f035de28a8f4a64e834f755b2b5c44a6df8
Varun Jagtap <cybervaruntech@gmail.com>
2022-11-06 11:42:26 +0530

    Update hacked.ps1

--- Powershell/hacked.ps1
+++ Powershell/hacked.ps1
@@ -1,6 +1,6 @@
 Write-Host "`n						You Have been Hacked`n" -ForegroundColor DarkGreen
 Write-Host "								By CyberVarun`n`n" -ForegroundColor DarkRed 
-Start-Sleep -Seconds 2
+# Start-Sleep -Seconds 2
 wt.exe
 Start-Sleep -Seconds 1
 wt.exe
